1c0517204e1b3744b62fba118193a1d20fa93e08
Hoto Cocoa (Lee Sung Jae) <cocoa@hoto.us>
2017-04-02 15:12:40 +0900

    security fix

--- doTwitterLogin.php
+++ doTwitterLogin.php
@@ -1,4 +1,11 @@
 <?php
+function giveHost($host_with_subdomain) {
+	$array = explode(".", $host_with_subdomain);
+	return (array_key_exists(count($array) - 2, $array) ? $array[count($array) - 2] : "").".".$array[count($array) - 1];
+}
+ini_set('session.cookie_domain', '.'.giveHost($_SERVER['SERVER_NAME']));
+session_start();
+
 require "vendor/autoload.php";
 use Abraham\TwitterOAuth\TwitterOAuth;
 $config = require_once 'config.php';
@@ -8,10 +15,10 @@ $connection = new TwitterOAuth($consumer_key, $consumer_secret);
 $request_token = $connection->oauth("oauth/request_token", array("oauth_callback" => $config['api_root'] . 'getTwitterData.php'));
 $oauth_token=$request_token['oauth_token'];
 $token_secret=$request_token['oauth_token_secret'];
-setcookie("token_secret", " ", time()-3600);
-setcookie("token_secret", $token_secret, time()+60*10);
-setcookie("oauth_token", " ", time()-3600);
-setcookie("oauth_token", $oauth_token, time()+60*10);
+unset($_COOKIE['oauth_token']);
+unset($_COOKIE['token_secret']);
+$_SESSION['oauth_token'] = $oauth_token;
+$_SESSION['token_secret'] = $token_secret;
 $url = $connection->url("oauth/authorize", array("oauth_token" => $oauth_token));
 header('Location: ' . $url);
 ?>
--- getTwitterData.php
+++ getTwitterData.php
@@ -4,6 +4,7 @@ function giveHost($host_with_subdomain) {
 	return (array_key_exists(count($array) - 2, $array) ? $array[count($array) - 2] : "").".".$array[count($array) - 1];
 }
 ini_set('session.cookie_domain', '.'.giveHost($_SERVER['SERVER_NAME']));
+
 require "vendor/autoload.php";
 use Abraham\TwitterOAuth\TwitterOAuth;
 $config = require_once 'config.php';
@@ -16,8 +17,8 @@ if($referrer !== "https://api.twitter.com/oauth/authorize") {
 	$consumer_key = $config['consumer_key'];
 	$consumer_secret = $config['consumer_secret'];
 	$oauth_verifier = $_GET['oauth_verifier'];
-	$token_secret = $_COOKIE['token_secret'];
-	$oauth_token = $_COOKIE['oauth_token'];
+	$token_secret = $_SESSION['token_secret'];
+	$oauth_token = $_SESSION['oauth_token'];
 	$connection = new TwitterOAuth($consumer_key, $consumer_secret, $oauth_token, $token_secret);
 	$access_token = $connection->oauth("oauth/access_token", array("oauth_verifier" => $oauth_verifier));
 	$accessToken=$access_token['oauth_token'];
