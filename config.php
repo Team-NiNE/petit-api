3c78f1ce56c841e40d72340560b7011e7acfbdc3
Hoto Cocoa (Lee Sung Jae) <cocoa@hoto.us>
2017-04-02 14:30:22 +0900

    miss!

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
 return [
 	'consumer_key'      => 'Ut8PZ4p05IjEncLqrr99ACtgg',
 	'consumer_secret'   => '7LHT02kr7HpaGxczGQWN9ctHZpP795ECmR1TFtY1fKkmdgTOso',
-	'root_location'     => 'https://localhost.twimg.com/osu-petit/apply-site/',
+	'root_location'     => 'https://osu.life/',
 	'api_root'        => 'https://api.osu.life/'
 ];
